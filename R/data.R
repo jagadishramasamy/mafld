2f3a20066a1251657d0950691dc76c24d35fc328
Jagadish Ramasamy <iamjagankmr@gmail.com>
2025-09-09 21:10:24 +0530

    3rd Submission commit

--- R/data.R
+++ R/data.R
@@ -1,14 +1,14 @@
 #' @title An example data frame
 #' @description A data frame with exact column names as specified
-#' @format A data frame with parameters needed to diagnose MAFLD.
+#' @format A data frame with parameters needed to diagnose MAFLD:
 #' \describe{
-#'   \item{CAP}{Controlled Attenuation Parameter, in dB/m}
-#'   \item{BMI}{Body mass index, in kg/m2}
-#'   \item{Race} {either Caucasians or Asians}
+#'   \item{CAP}{Controlled Attenuation Parameter in dB/m}
+#'   \item{BMI}{Body mass index in kg/m2}
+#'   \item{Race}{either Caucasians or Asians}
 #'   \item{Gender}{Gender in Male or Female}
 #'   \item{WC}{Waist Circumference in cm}
 #'   \item{FPG}{Fasting plasma glucose in mg/dL}
-#'   \item{HbA1c}{Glycated Hemoglobin, in %}
+#'   \item{HbA1C}{Glycated Hemoglobin (in\%)}
 #'   \item{SBP}{Systolic BP in mm Hg}
 #'   \item{DBP}{Diastolic BP in mm Hg}
 #'   \item{HDL}{High Density Lipoprotein Cholesterol in mg/dL}
